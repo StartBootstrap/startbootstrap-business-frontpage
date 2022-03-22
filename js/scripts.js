6654c76d6f4e6f70f51806c6ca378bf4ebcc8551
David Miller <hello@davidmiller.io>
2022-03-22 16:35:01 -0400

    dependencies update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Business Frontpage v5.0.7 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
 // This file is intentionally blank
