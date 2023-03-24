83c090252e3dd797d8af72ea6e7639656f41ea01
David Miller <hello@davidmiller.io>
2023-03-24 18:40:30 -0400

    Bootstrap 5.2.3 dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Business Frontpage v5.0.8 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
 // This file is intentionally blank
